a6c83e7481dabfdef855fbab918012a43b1ca851
Mehdi <mehdirahimi@email.com>
2022-06-22 18:11:39 +0330

    Add default empty Spring Boot files.

---
+++ src/main/java/ir/airglow/ezbudget/EzbudgetApplication.java
@@ -0,0 +1,13 @@
+package ir.airglow.ezbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzbudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzbudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/airglow/ezbudget/EzbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.airglow.ezbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
