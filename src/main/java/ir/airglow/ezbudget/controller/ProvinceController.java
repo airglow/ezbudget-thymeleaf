3f256b912c56dbc188f0f0fcd496cec08d39ef6e
mehdirahimi <mehdirahimi@email.com>
2022-06-25 14:56:53 +0430

    Inject Province Service to Controller and use it inside template page.

--- src/main/java/ir/airglow/ezbudget/controller/ProvinceController.java
+++ src/main/java/ir/airglow/ezbudget/controller/ProvinceController.java
@@ -1,17 +1,35 @@
 package ir.airglow.ezbudget.controller;
 
 import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ir.airglow.ezbudget.dto.ProvinceDto;
+import ir.airglow.ezbudget.service.ProvinceService;
 
 @Log4j2
 @Controller
 @RequestMapping("/provinces")
 public class ProvinceController {
 
+	private final ProvinceService provinceService;
+	
+	public ProvinceController(ProvinceService provinceService) {
+		this.provinceService = provinceService;
+	}
+	
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+    	
+    	List<ProvinceDto> provinces = provinceService.list();
+    	
+    	model.addAttribute("provinces", provinces);
+    	
         return "index";
     }
 
